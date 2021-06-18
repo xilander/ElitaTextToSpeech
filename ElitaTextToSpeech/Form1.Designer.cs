636214c2476dcc105e44c468cdc33878e1b3a7ce
Xilander <xilander@hotmail.com>
2021-06-18 10:06:59 -0500

    Commit Inicial

---
+++ ElitaTextToSpeech/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿
+namespace ElitaTextToSpeech
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.btn_Speak = new System.Windows.Forms.Button();
+            this.btn_Resume = new System.Windows.Forms.Button();
+            this.btn_Pause = new System.Windows.Forms.Button();
+            this.btn_Stop = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.textBoxFileName = new System.Windows.Forms.TextBox();
+            this.labelFileName = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.Location = new System.Drawing.Point(10, 10);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(654, 111);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // btn_Speak
+            // 
+            this.btn_Speak.Location = new System.Drawing.Point(21, 166);
+            this.btn_Speak.Name = "btn_Speak";
+            this.btn_Speak.Size = new System.Drawing.Size(96, 51);
+            this.btn_Speak.TabIndex = 1;
+            this.btn_Speak.Text = "Reproducir / Grabar";
+            this.btn_Speak.UseVisualStyleBackColor = true;
+            this.btn_Speak.Click += new System.EventHandler(this.btn_Speak_Click);
+            // 
+            // btn_Resume
+            // 
+            this.btn_Resume.Location = new System.Drawing.Point(227, 167);
+            this.btn_Resume.Name = "btn_Resume";
+            this.btn_Resume.Size = new System.Drawing.Size(96, 50);
+            this.btn_Resume.TabIndex = 2;
+            this.btn_Resume.Text = "Continuar";
+            this.btn_Resume.UseVisualStyleBackColor = true;
+            this.btn_Resume.Click += new System.EventHandler(this.btn_Resume_Click);
+            // 
+            // btn_Pause
+            // 
+            this.btn_Pause.Location = new System.Drawing.Point(123, 167);
+            this.btn_Pause.Name = "btn_Pause";
+            this.btn_Pause.Size = new System.Drawing.Size(96, 50);
+            this.btn_Pause.TabIndex = 3;
+            this.btn_Pause.Text = "Pausa";
+            this.btn_Pause.UseVisualStyleBackColor = true;
+            this.btn_Pause.Click += new System.EventHandler(this.btn_Pause_Click);
+            // 
+            // btn_Stop
+            // 
+            this.btn_Stop.Location = new System.Drawing.Point(558, 166);
+            this.btn_Stop.Name = "btn_Stop";
+            this.btn_Stop.Size = new System.Drawing.Size(96, 51);
+            this.btn_Stop.TabIndex = 4;
+            this.btn_Stop.Text = "Salir";
+            this.btn_Stop.UseVisualStyleBackColor = true;
+            this.btn_Stop.Click += new System.EventHandler(this.btn_Stop_Click_1);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(21, 129);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(90, 19);
+            this.checkBox1.TabIndex = 5;
+            this.checkBox1.Text = "Crear audio";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // textBoxFileName
+            // 
+            this.textBoxFileName.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxFileName.Location = new System.Drawing.Point(237, 127);
+            this.textBoxFileName.Name = "textBoxFileName";
+            this.textBoxFileName.Size = new System.Drawing.Size(103, 23);
+            this.textBoxFileName.TabIndex = 6;
+            // 
+            // labelFileName
+            // 
+            this.labelFileName.AutoSize = true;
+            this.labelFileName.Location = new System.Drawing.Point(119, 130);
+            this.labelFileName.Name = "labelFileName";
+            this.labelFileName.Size = new System.Drawing.Size(117, 15);
+            this.labelFileName.TabIndex = 7;
+            this.labelFileName.Text = "Nombre del archivo:";
+            this.labelFileName.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(677, 231);
+            this.Controls.Add(this.labelFileName);
+            this.Controls.Add(this.textBoxFileName);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.btn_Stop);
+            this.Controls.Add(this.btn_Pause);
+            this.Controls.Add(this.btn_Resume);
+            this.Controls.Add(this.btn_Speak);
+            this.Controls.Add(this.richTextBox1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Elita Conversor de texto a audio";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button btn_Speak;
+        private System.Windows.Forms.Button btn_Resume;
+        private System.Windows.Forms.Button btn_Pause;
+        private System.Windows.Forms.Button btn_Stop;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.TextBox textBoxFileName;
+        private System.Windows.Forms.Label labelFileName;
+    }
+}
+
---
+++ ElitaTextToSpeech/Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Speech;
+using System.Speech.AudioFormat;
+using System.Speech.Synthesis;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace ElitaTextToSpeech
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //SpeechSynthesizer Class Provides access to the functionality of an installed a speech synthesis engine.   
+        SpeechSynthesizer speechSynthesizerObj;
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            speechSynthesizerObj = new SpeechSynthesizer();
+            btn_Resume.Enabled = false;
+            btn_Pause.Enabled = false;
+            labelFileName.Visible = false;
+            textBoxFileName.Visible = false;
+        }
+        private void btn_Speak_Click(object sender, EventArgs e)
+        {
+            //Disposes the SpeechSynthesizer object   
+            //speechSynthesizerObj.Dispose();
+            try
+            {
+                if (richTextBox1.Text != "")
+                {
+                    speechSynthesizerObj = new SpeechSynthesizer();
+                    //Asynchronously speaks the contents present in RichTextBox1
+                    speechSynthesizerObj.SelectVoice("Microsoft Sabina Desktop");
+
+                    if (checkBox1.Checked)
+                    {
+                        string filePath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+
+                        if (textBoxFileName.Text != "")
+                        {
+                            string fileName = textBoxFileName.Text;
+                            filePath = filePath + "\\" + fileName + ".wav";
+                            speechSynthesizerObj.SetOutputToWaveFile(filePath, new SpeechAudioFormatInfo(32000, AudioBitsPerSample.Sixteen, AudioChannel.Mono));
+                            MessageBox.Show("Archivo wav generado, revise su escritorio", "Muy bien...");
+                        }
+                        else
+                        {
+                            MessageBox.Show("No ha ingresado el nombre del archivo", "Atención...");
+                        }
+
+                        speechSynthesizerObj.SpeakAsync(richTextBox1.Text);
+                        btn_Pause.Enabled = true;
+                        btn_Stop.Enabled = true;
+                    }
+                    else
+                    {
+                        speechSynthesizerObj.SpeakAsync(richTextBox1.Text);
+                        btn_Pause.Enabled = true;
+                        btn_Stop.Enabled = true;
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("No hay texto a sintetizar", "Atención...");
+                }
+            }
+            catch (Exception ex)
+            {
+                long caseSwitch = Math.Abs(ex.HResult);
+
+                switch (caseSwitch)
+                {
+                    case 2147024809:
+                        MessageBox.Show(ex.Message, "Se ha producido un error !!!");
+                        break;
+                    case 2147024864:
+                        MessageBox.Show(ex.Message + "\n \n Intente con otro nombre", "Se ha producido un error !!!");
+                        break;
+                    default:
+                        MessageBox.Show(ex.Message, "Se ha producido un error !!!");
+                        break;
+                }
+            }
+        }
+        private void btn_Pause_Click(object sender, EventArgs e)
+        {
+            if (speechSynthesizerObj != null)
+            {
+                //Gets the current speaking state of the SpeechSynthesizer object.   
+                if (speechSynthesizerObj.State == SynthesizerState.Speaking)
+                {
+                    //Pauses the SpeechSynthesizer object.   
+                    speechSynthesizerObj.Pause();
+                    btn_Resume.Enabled = true;
+                    btn_Speak.Enabled = false;
+                }
+            }
+        }
+        private void btn_Resume_Click(object sender, EventArgs e)
+        {
+            if (speechSynthesizerObj != null)
+            {
+                if (speechSynthesizerObj.State == SynthesizerState.Paused)
+                {
+                    //Resumes the SpeechSynthesizer object after it has been paused.   
+                    speechSynthesizerObj.Resume();
+                    btn_Resume.Enabled = false;
+                    btn_Speak.Enabled = true;
+                }
+            }
+        }
+        private void btn_Stop_Click_1(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                labelFileName.Visible = true;
+                textBoxFileName.Visible = true;
+            }
+            else
+            {
+                labelFileName.Visible = false;
+                textBoxFileName.Visible = false;
+            }
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
